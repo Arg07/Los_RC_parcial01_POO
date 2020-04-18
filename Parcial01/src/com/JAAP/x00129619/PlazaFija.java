841b67bfc910127ccdeab0ad4140de641af8adda
00129619 <00129619@uca.edu.sv>
2020-04-18 14:45:28 -0600

    Edit: PlazaFija. Add: Constructor and methods Getter & Setter

--- Parcial01/src/com/JAAP/x00129619/PlazaFija.java
+++ Parcial01/src/com/JAAP/x00129619/PlazaFija.java
@@ -1,4 +1,19 @@
 package com.JAAP.x00129619;
 
-public class PlazaFija {
+import java.util.ArrayList;
+
+public class PlazaFija extends Empleado{
+    private int Extension;
+
+    public PlazaFija(String nombre, String puesto, ArrayList<Documento> documentos, double salario,  int extension) {
+        super(nombre, puesto, documentos, salario);
+        this.Extension =  extension;
+    }
+    // Getter & Setter de Extension
+    public int getExtension() {
+        return Extension;
+    }
+    public void setExtension(int extension) {
+        Extension = extension;
+    }
 }
