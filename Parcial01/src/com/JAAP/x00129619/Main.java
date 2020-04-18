289d43ae494d8a8b59e21206ebe9c05cdb93905e
00129619 <00129619@uca.edu.sv>
2020-04-18 15:14:17 -0600

    Eddit: Main class. Add: MainMenu

--- Parcial01/src/com/JAAP/x00129619/Main.java
+++ Parcial01/src/com/JAAP/x00129619/Main.java
@@ -1,8 +1,40 @@
 package com.JAAP.x00129619;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-	// write your code here
+
+        byte Menuop = 0;
+        do{
+            MainMenu();
+            Menuop =  sc.nextByte();sc.nextLine();
+            switch (Menuop){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opcion Invalida");
+                    break;
+            }
+        }while(Menuop != 0);
+    }
+    static void MainMenu(){
+        System.out.println("1) Agregar Empleado.");
+        System.out.println("2) Despedir Empleado.");
+        System.out.println("3) Ver lista de empleados.");
+        System.out.println("4) Calcular sueldo.");
+        System.out.println("5) Mostrar totales.");
+        System.out.print("Opcion: ");
     }
 }
